98346d9b7742ebea8f0cf5c1c66a54b0c6721969
LUIS ALFONSO AGUILAR OBANDO <ing.luis.aguilar.obando@gmail.com>
2022-10-22 20:23:28 -0500

    update sabado

--- index.js
+++
@@ -1,8 +0,0 @@
-let app = require('./src/app');
-let connection = require('./src/db/connection');
-
-const PORT = 4000;
-
-app.listen( PORT, ()=> {
-    console.log("Server ready on port", PORT);
-});
